0cc84d16ec73960d0fe567e0054e1854fe3ab580
onjamm <azmionjang@gmail.com>
2026-09-23 20:44:02 -0700

    Level count has passed!

--- src/Practice.java
+++ src/Practice.java
@@ -149,7 +149,11 @@ public class Practice {
      * @return the number of levels in the tree
      */
     public static int levelCount(BinaryTreeNode<?> root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        } 
+
+        return 1 + Math.max(levelCount(root.left), levelCount(root.right));
     }
 
 
